7bbd083e7bf821f9621b375be4185595114772c0
AhRastgar <ah.rastgar.c@gmail.com>
2022-05-06 13:14:32 +0330

    Show Stars

---
+++ ShowStars.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class ShowStars
+{
+  public static void main(String[] args)
+  {
+    System.out.println("This project is for check golden number.");
+
+    Scanner x = new Scanner(System.in);
+    System.out.println("Enter your number ");
+    int a = x.nextInt();
+
+    for (int i = 0; i < a; i++)
+    {
+      System.out.print("* ");
+    }
+  }
+}
