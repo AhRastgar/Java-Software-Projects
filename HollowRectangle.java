ad3d749af6ef549973c61f9c11acb42bafc95128
AhRastgar <ah.rastgar.c@gmail.com>
2022-05-14 09:56:38 +0330

    Hollow rectangle

---
+++ HollowRectangle.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class HollowRectangle extends JPanel
+{
+  public static void main(String[] a)
+  {
+    JFrame r = new JFrame();
+    r.setSize(400, 400);
+    r.add(new DrawRectangle());
+    r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    r.setVisible(true);
+  }
+
+  public void paint(Graphics g)
+  {
+    g.drawRect(5, 15, 200, 100);
+  }
+}
